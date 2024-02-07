f6cbca1f13adb1e5fc0dbd866bc308a2674b5b42
malikmm <malikmm@hotmail.com>
2024-02-07 19:08:34 +0000

    initial setup

---
+++ src/main/java/com/malik/springstartergradle/SpringstartergradleApplication.java
@@ -0,0 +1,13 @@
+package com.malik.springstartergradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringstartergradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringstartergradleApplication.class, args);
+	}
+
+}
